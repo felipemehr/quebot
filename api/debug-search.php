2ff3868282d6cb348523d635bc95194372e17be5
Felipe Mehr <felipemehr@gmail.com>
2026-02-17 08:55:05 -0300

    Improved debug to find result patterns

--- api/debug-search.php
+++ api/debug-search.php
@@ -1,10 +1,7 @@
 <?php
-/**
- * Debug: ver HTML de DuckDuckGo
- */
 header('Content-Type: text/plain; charset=utf-8');
 
-$query = $_GET['q'] ?? 'parcelas melipeuco';
+$query = $_GET['q'] ?? 'parcelas';
 $url = 'https://html.duckduckgo.com/html/?q=' . urlencode($query);
 
 $ch = curl_init();
@@ -17,13 +14,33 @@ curl_setopt_array($ch, [
 ]);
 
 $html = curl_exec($ch);
-$error = curl_error($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
 echo "Query: $query\n";
-echo "URL: $url\n";
-echo "HTTP Code: $httpCode\n";
-echo "Error: $error\n\n";
-echo "=== Primeros 5000 chars del HTML ===\n\n";
-echo substr($html, 0, 5000);
+echo "HTML Length: " . strlen($html) . "\n\n";
+
+// Buscar clases de resultado
+echo "=== Clases encontradas ===\n";
+preg_match_all('/class="([^"]*result[^"]*)"/i', $html, $classes);
+print_r(array_unique($classes[1]));
+
+echo "\n=== Buscando <a con result__a ===\n";
+preg_match_all('/<a[^>]+class="[^"]*result__a[^"]*"[^>]*>([^<]{0,100})/i', $html, $m1);
+print_r($m1[1]);
+
+echo "\n=== Buscando href con duckduckgo ===\n";
+preg_match_all('/href="([^"]*duckduckgo[^"]*uddg[^"]*)"/i', $html, $m2);
+print_r(array_slice($m2[1], 0, 5));
+
+echo "\n=== Fragment donde aparece 'result' ===\n";
+$pos = strpos($html, 'result__');
+if ($pos !== false) {
+    echo substr($html, $pos - 50, 500);
+} else {
+    echo "No se encontró 'result__' en el HTML\n";
+    echo "\n=== Buscando otras estructuras ===\n";
+    $pos2 = strpos($html, '<div class="links');
+    if ($pos2 !== false) {
+        echo substr($html, $pos2, 500);
+    }
+}
