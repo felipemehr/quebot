9f5726a53fed7515f7790df1072e6f431d036bce
Felipe Mehr <felipemehr@gmail.com>
2026-02-17 05:59:46 +0000

    Fix env variable reading

--- api/config.php
+++ api/config.php
@@ -1,3 +1,12 @@
 <?php
-define('CLAUDE_API_KEY', getenv('CLAUDE_API_KEY') ?: '');
+// Try multiple methods to get env variable
+$apiKey = getenv('CLAUDE_API_KEY');
+if (empty($apiKey)) {
+    $apiKey = $_ENV['CLAUDE_API_KEY'] ?? '';
+}
+if (empty($apiKey)) {
+    $apiKey = $_SERVER['CLAUDE_API_KEY'] ?? '';
+}
+
+define('CLAUDE_API_KEY', $apiKey);
 define('CLAUDE_MODEL', 'claude-sonnet-4-20250514');
