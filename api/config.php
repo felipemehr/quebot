17945ae040ea0b86d7cdb2eb7cf8fee6301358de
Felipe Mehr <felipemehr@gmail.com>
2026-02-17 06:06:35 +0000

    Add status endpoint

--- api/config.php
+++ api/config.php
@@ -1,12 +1,5 @@
 <?php
-// Try multiple methods to get env variable
-$apiKey = getenv('CLAUDE_API_KEY');
-if (empty($apiKey)) {
-    $apiKey = $_ENV['CLAUDE_API_KEY'] ?? '';
-}
-if (empty($apiKey)) {
-    $apiKey = $_SERVER['CLAUDE_API_KEY'] ?? '';
-}
-
+$apiKey = getenv('CLAUDE_API_KEY') ?: ($_ENV['CLAUDE_API_KEY'] ?? '');
 define('CLAUDE_API_KEY', $apiKey);
 define('CLAUDE_MODEL', 'claude-sonnet-4-20250514');
+define('API_CONFIGURED', !empty($apiKey));
